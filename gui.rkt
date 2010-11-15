d2e7ee16c9f4300ac11239e462cdc79a5bc2c4be
Dmitry Chestnykh <dmitry@codingrobots.com>
2010-11-15 02:33:48 +0100

    Reformat GUI code.

--- gui.rkt
+++ gui.rkt
@@ -2,31 +2,35 @@
 
 (require "bayes.rkt")
 
-(define frame (new frame% [label "I Write Like"]
+(define frame (new frame% 
+                   [label "I Write Like"]
                    [spacing 4]
                    [border 10]
                    [min-width 400]
                    [min-height 400]))
 
 (define loading-msg (new message%
-                     [parent frame]
-                     [label "Loading data..."]))
+                         [parent frame]
+                         [label "Loading data..."]))
 
 (send frame show #t)
 (load-data!)
 (send frame delete-child loading-msg)
 
-(define text-field (new text-field% [parent frame]
-                          [label "Text"]
-                          [style '(multiple vertical-label)]))
+(define text-field (new text-field% 
+                        [parent frame]
+                        [label "Text"]
+                        [style '(multiple vertical-label)]))
 
 (new button% [parent frame]
      [label "Analyze"]
      (callback (lambda (button event)
-                   (send result-field set-value (get-category (send text-field get-value)))
-                   (or (member result-field (send frame get-children))
-                       (send frame add-child result-field)))))
+                 (send result-field set-value 
+                       (get-category (send text-field get-value)))
+                 (or (member result-field (send frame get-children))
+                     (send frame add-child result-field)))))
 
-(define result-field (new text-field% [parent frame]
-                            [label "Written like"]
-                            [style '(single deleted vertical-label)]))
+(define result-field (new text-field% 
+                          [parent frame]
+                          [label "Written like"]
+                          [style '(single deleted vertical-label)]))
