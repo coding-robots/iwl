d40bb1466aa221cd951648bb76572f7d7ab96f8b
Dmitry Chestnykh <dmitry@codingrobots.com>
2010-11-26 16:15:55 +0100

    Add crc32 notice.

--- crc32.rkt
+++ crc32.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
-; -- Taken from unlib/crc, under LGPL
+; crc32 function taken from unlib/crc, licensed under LGPL,
+; Copyright (C) 2008 Untyped Ltd.
 ; http://planet.racket-lang.org/display.ss?package=unlib.plt&owner=untyped
+;
+; Alternatively, you can remove it and uncomment the following line:
+; (require (planet untyped/unlib/crc))
 
 (define (crc32 data)
   (bitwise-xor
@@ -12,6 +16,8 @@
        (bitwise-xor (quotient accum 2)
                     (* #xEDB88320 (bitwise-and accum 1)))))
    #xFFFFFFFF))
+
+; ---
 
 (define (string->crc32 s)
   (crc32 (string->bytes/utf-8 s)))
